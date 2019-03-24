c86cfe2cc03290a7e594af295abccb5b0bfbdbf4
Ilya Puchka <ilyapuchka@gmail.com>
2019-03-24 11:52:07 +0000

    support up to 10 parameters

--- Sources/CLImate/CLImate.swift
+++ Sources/CLImate/CLImate.swift
@@ -374,3 +374,27 @@ public func <¢><A, B, C, U: Matchable>(_ f: @escaping (A, B, C) -> U, cli: CLI<
 public func <¢><A, B, C, D, U: Matchable>(_ f: @escaping (A, B, C, D) -> U, cli: CLI<(A, (B, (C, D)))>) -> CLI<U> {
     return iso(f) <¢> cli
 }
+
+public func <¢><A, B, C, D, E, U: Matchable>(_ f: @escaping (A, B, C, D, E) -> U, cli: CLI<(A, (B, (C, (D, E))))>) -> CLI<U> {
+    return iso(f) <¢> cli
+}
+
+public func <¢><A, B, C, D, E, F, U: Matchable>(_ f: @escaping (A, B, C, D, E, F) -> U, cli: CLI<(A, (B, (C, (D, (E, F)))))>) -> CLI<U> {
+    return iso(f) <¢> cli
+}
+
+public func <¢><A, B, C, D, E, F, G, U: Matchable>(_ f: @escaping (A, B, C, D, E, F, G) -> U, cli: CLI<(A, (B, (C, (D, (E, (F, G))))))>) -> CLI<U> {
+    return iso(f) <¢> cli
+}
+
+public func <¢><A, B, C, D, E, F, G, H, U: Matchable>(_ f: @escaping (A, B, C, D, E, F, G, H) -> U, cli: CLI<(A, (B, (C, (D, (E, (F, (G, H)))))))>) -> CLI<U> {
+    return iso(f) <¢> cli
+}
+
+public func <¢><A, B, C, D, E, F, G, H, I, U: Matchable>(_ f: @escaping (A, B, C, D, E, F, G, H, I) -> U, cli: CLI<(A, (B, (C, (D, (E, (F, (G, (H, I))))))))>) -> CLI<U> {
+    return iso(f) <¢> cli
+}
+
+public func <¢><A, B, C, D, E, F, G, H, I, J, U: Matchable>(_ f: @escaping (A, B, C, D, E, F, G, H, I, J) -> U, cli: CLI<(A, (B, (C, (D, (E, (F, (G, (H, (I, J)))))))))>) -> CLI<U> {
+    return iso(f) <¢> cli
+}
